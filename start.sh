4bd0ad721d88aa8a48c0f95225df2603348096ea
kkloberdanz <kyle.g.kloberdanz@gmail.com>
2018-11-15 18:51:36 -0600

    fixed server

--- start.sh
+++ start.sh
@@ -2,8 +2,10 @@
 
 
 ps -ef | grep "gunicorn" | grep -v grep | awk '{ print $2 }' | xargs kill
+ps -ef | grep "python3" | grep "app.py" | grep -v grep | awk '{ print $2 }' | xargs kill
 
 sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 5000
 source /home/pi/venv/bin/activate
 cd /home/pi/SecurityCam/
-nohup gunicorn --workers 4 --bind 192.168.0.103:5000 app:app &
+#nohup gunicorn --workers 4 --bind 192.168.0.103:5000 app:app &
+nohup python3 /home/pi/SecurityCam/app.py &
