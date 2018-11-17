3e6f1d493d0378f234875f7a0adc5688424665ca
kkloberdanz <kyle.g.kloberdanz@gmail.com>
2018-11-17 14:41:37 -0600

    moved redis server to another machine

--- pycam.py
+++ pycam.py
@@ -3,7 +3,7 @@ import time
 import redis
 
 
-redis_conn = redis.Redis(host='127.0.0.1',
+redis_conn = redis.Redis(host='192.168.0.110',
                          port='6379')
 
 
@@ -15,7 +15,6 @@ def stream():
             yield pic
             if time.time() - start > 600:  # 10 minute timeout
                 raise Exception('End of stream')
-            time.sleep(0.05)  # limit frame-rate to <24 FPS
 
     except Exception as e:
         pic = cv2.imread('end_of_stream.jpg')
